6ce1d5486a9520460c7f51e6c62dcffbb60c1f70
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-15 23:16:44 +0700

    Add store with categories and fetch it at init

---
+++ store/index.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  categories: []
+})
+
+export const getters = {
+  categories: state => state.categories
+}
+
+export const mutations = {
+  SET_CATEGORIES (state, categories) {
+    state.categories = categories
+  }
+}
+
+export const actions = {
+  async nuxtServerInit ({ commit }) {
+    const response = await this.$axios.$get('categories')
+    commit('SET_CATEGORIES', response.data)
+  }
+}

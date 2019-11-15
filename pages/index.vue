e1dcb9cb32dd2f39df051330951968f1b957ce15
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-15 23:19:13 +0700

    Default layout and main page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- <Navigation /> -->
+    <div class="container">
+      <div>
+        <h1 class="title">
+          client updated
+        </h1>
+        <h2 class="subtitle">
+          fullstack ecommerce is coming
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>

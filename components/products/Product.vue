1799640e6d777b312ed4d0cffcd9eea4d111cf0a
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-16 20:45:11 +0700

    Add dynamic page for products of category with Product component

---
+++ components/products/Product.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :key="product.slug" class="card" style="max-width: 300px">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <nuxt-link :to="linkToDetail">
+          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        </nuxt-link>
+      </figure>
+    </div>
+    <div class="card-content">
+      <nuxt-link :to="linkToDetail">
+        <p class="title is-4">{{ product.name }}</p>
+      </nuxt-link>
+
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus nec iaculis mauris
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    linkToDetail () {
+      return { name: 'products-slug', params: { slug: this.product.slug } }
+    }
+  },
+  props: {
+    product: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
--- pages/categories/_slug.vue
+++ pages/categories/_slug.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    This is one specific category
+  <div class="container" style="margin-top: 2rem;">
+    <template v-for="product in products">
+      <div :key="product.slug" class="card" style="max-width: 300px">
+        <Product :product="product"/>
+      </div>
+    </template>
   </div>
 </template>
+
+<script>
+import Product from '~/components/products/Product'
+
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  components: {
+    Product
+  },
+  async asyncData ({ params, app }) {
+    const response = await app.$axios.$get(`products?category=${params.slug}`)
+    return { products: response.data }
+  },
+  computed: {
+    linkToDetail () {
+      return { name: 'products-slug', params: { slug: this.product.slug } }
+    }
+  }
+}
+</script>

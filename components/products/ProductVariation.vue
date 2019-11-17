afb0f0ae2425e86689e3bc8ff74a1f33c107d2f4
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-17 23:22:43 +0700

    Tell parent when variation selected

--- components/products/ProductVariation.vue
+++ components/products/ProductVariation.vue
@@ -3,8 +3,8 @@
     <label class="label">{{ type }}</label>
     <div class="control">
       <div class="select is-fullwidth">
-        <select>
-          <option>Select Options</option>
+        <select :value="selectedValue" @change="changed($event, type)">
+          <option value="">Select Options</option>
           <option
             v-for="variation in variations"
             :key="variation.id"
@@ -20,6 +20,14 @@
 
 <script>
 export default {
+  computed: {
+    selectedValue () {
+      if (!this.value) {
+        return ''
+      }
+      return this.findVariation(this.value.id) ? this.value.id : ''
+    }
+  },
   props: {
     type: {
       required: true,
@@ -28,7 +36,26 @@ export default {
     variations: {
       required: true,
       type: Array
+    },
+    value: {
+      required: false,
+      default: ''
     }
+  },
+  methods: {
+    changed (event, type) {
+      const variation = this.findVariation(parseInt(event.target.value))
+      if (!variation) {
+        return
+      }
+      this.$emit('selectVariation', variation)
+    },
+    findVariation (id) {
+      return this.variations.find(variation => id === variation.id)
+    }
+  },
+  model: {
+    event: 'selectVariation' // default: input
   }
 }
 </script>

a43f88386506b8f6faa984deaf260ab907a49dee
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-20 21:54:11 +0700

    Add plugin to log out axios error

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    '~/plugins/axiosInterceptor'
+  ],
   /*
    ** Nuxt.js dev-modules
    */

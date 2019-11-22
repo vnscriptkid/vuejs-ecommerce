80ba327e1e6b0f5b2632a58aa2ec6760c9929504
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-23 03:06:50 +0700

    Add checkout page, reuse CartOverview inside

---
+++ pages/checkout/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <h1 class="title">Checkout</h1>
+    <div class="columns">
+      <div class="column is-three-quarters">
+        <article class="message is-dark">
+          <div class="message-header">
+            <p>Ship To</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+          </div>
+        </article>
+        <article class="message is-dark">
+          <div class="message-header">
+            <p>Payment</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+          </div>
+        </article>
+        <article class="message is-dark">
+          <div class="message-header">
+            <p>Shipping</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+          </div>
+        </article>
+        <article class="message is-dark" v-if="cartSize">
+          <div class="message-header">
+            <p>Cart Summary</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            <CartOverview />
+          </div>
+        </article>
+      </div>
+      <div class="column is-one-quarters">
+        <button :disabled="cartSize === 0" class="button is-success is-fullwidth">Checkout</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import CartOverview from '~/components/cart/CartOverview'
+
+export default {
+  components: {
+    CartOverview
+  },
+  computed: {
+    ...mapGetters({
+      cartSize: 'cart/cartSize'
+    })
+  }
+}
+</script>

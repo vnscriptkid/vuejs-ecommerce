edcb4a749d8020d9cbbfcd99e1fc537529688fce
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-23 12:03:45 +0700

    User can change default shipping address locally

---
+++ components/checkout/addresses/AddressSelector.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <table class="table is-fullwidth is-hoverable is-dark">
+      <tbody>
+        <tr v-for="address in addresses" :key="address.id">
+          <td :class="isSelected(address) ? 'has-text-weight-bold' : ''">
+            {{ address.id }}
+            {{ address.name }} <br>
+            {{ address.address_1 }} <br>
+            {{ address.city }} <br>
+            {{ address.postal_code }} <br>
+            {{ address.country.name }} <br>
+          </td>
+          <td>
+            <button @click="$emit('select', address)" v-if="!isSelected(address)" class="button is-success">Ship To</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    addresses: {
+      required: true,
+      type: Array
+    },
+    selectedAddress: {
+      optional: true,
+      type: Object
+    }
+  },
+  methods: {
+    isSelected (address) {
+      return address.id === this.selectedAddress.id
+    }
+  }
+}
+</script>
--- components/checkout/addresses/ShippingAddress.vue
+++ components/checkout/addresses/ShippingAddress.vue
@@ -1,23 +1,53 @@
 <template>
   <article class="message is-dark">
+    <!-- header -->
     <div class="message-header">
       <p>Ship To</p>
       <button class="delete" aria-label="delete"></button>
     </div>
+
     <div class="message-body">
-      <!-- {{ localAddresses }} -->
-      <!-- {{ defaultAddress }} -->
-      {{ selectedAddress }}
+      <!-- default address -->
+      <p v-if="!selecting">
+        {{ selectedAddress.name }} <br>
+        {{ selectedAddress.address_1 }} <br>
+        {{ selectedAddress.city }} <br>
+        {{ selectedAddress.postal_code }} <br>
+        {{ selectedAddress.country.name }} <br>
+      </p>
+
+      <!-- address selections -->
+      <AddressSelector
+        v-else
+        :addresses="localAddresses"
+        :selectedAddress="selectedAddress"
+        @select="switchSelectedAddress"
+      />
+
+      <!-- change to selections mode -->
+      <div v-if="!selecting" class="field is-grouped" style="margin-top: 1.5rem;">
+        <p class="control">
+          <button @click="toggleSelecting" class="button is-primary">
+            Change shipping address
+          </button>
+        </p>
+      </div>
     </div>
   </article>
 </template>
 
 <script>
+import AddressSelector from './AddressSelector'
+
 export default {
+  components: {
+    AddressSelector
+  },
   data () {
     return {
       localAddresses: this.addresses,
-      selectedAddress: null
+      selectedAddress: null,
+      selecting: false
     }
   },
   props: {
@@ -32,12 +62,16 @@ export default {
     }
   },
   methods: {
-    selectAddress (address) {
+    switchSelectedAddress (address) {
       this.selectedAddress = address
+      this.selecting = false
+    },
+    toggleSelecting () {
+      this.selecting = !this.selecting
     }
   },
   created () {
-    this.selectAddress(this.defaultAddress)
+    this.switchSelectedAddress(this.defaultAddress)
   }
 }
 </script>
